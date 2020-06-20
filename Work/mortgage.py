b1654194c109c20b0190a9c025a46d0f503ef62f
Ventsislav Kaynakchiev <vkayn@softserveinc.com>
2020-06-20 20:17:17 +0300

    Ex. 1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -13,12 +13,13 @@ extra_payment = 1000
 
 month = 0
 while principal > 0:
+    month += 1
     principal = principal * (1+rate/12) - payment
     if extra_payment_start_month <= month < extra_payment_end_month:
         principal = principal - extra_payment
         total_paid = total_paid + extra_payment
     total_paid = total_paid + payment
-    month += 1
+    print(month, f'{total_paid:,.2f}', f'{principal:,.2f}')
 
 print('Total paid',f'{total_paid:,.2f}')
 print('Months', month)
